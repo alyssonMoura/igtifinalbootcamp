6d5aefb483ed9963946de2ddda6f3f87c92e397b
alyssonMoura <alyssonmoura367@gmail.com>
2020-07-21 18:26:49 -0300

    Correção de url base

--- client/src/service/service.js
+++ client/src/service/service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://bootcamp-final-igti.herokuapp.com/",
   headers: {
     "Content-type": "application/json",
   },
